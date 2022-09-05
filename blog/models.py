7a9517d1c6e380f7699f3bd2d6afedeb9c88f363
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-05 20:58:23 +0300

    models and admin.py were created

--- blog/models.py
+++ blog/models.py
@@ -1,30 +1,20 @@
 from django.db import models
 
-from users.models import Users
 
 
-class Post(Users):
+
+class Post(models.Model):
     title= models.CharField(max_length=70)
+    slug=models.CharField(max_length=70)
     content=models.TextField()
-    image=models.ImageField(upload_to='profile-pics', height_field=None, width_field=None, blank=true)
+    image=models.ImageField(upload_to='profile-pics', height_field=None, width_field=None, blank=True)
     publish_date=models.DateTimeField(auto_now_add=True)
     last_updated=models.DateTimeField(auto_now=True)
     status=models.CharField(max_length=70)
-    slug=models.IntegerField()
 
     def __str__(self):
         return self.title
-class Category(Post):
-    name=models.CharField(max_length=70)   
 
 
-class Comment(Post):
-    # time_stamp=models.DateTimeField(auto_now_add=True)
-    # last_updated=models.DateTimeField(auto_now=True)
-    # content=models.TextField()
 
-class Like(Post):
-
-class PostView(Post):
-    #  time_stamp=models.DateTimeField(auto_now_add=True)
 
