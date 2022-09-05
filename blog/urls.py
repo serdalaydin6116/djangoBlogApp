b53aee188b6f13b82523a92ec27e9d722aea9e01
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-06 00:48:51 +0300

    blog app files

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import home
+
+urlpatterns=[
+    path('', home, name='home'),
+    # path('add/', todo_create, name='add'),
+    # path('update/<int:id>', todo_update, name='update'),
+    # path('delete/<int:id>', todo_delete, name='delete')
+
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post 
+from .forms import PostForm
+from django.contrib import messages
 
 # Create your views here.
+
+def home(request):    
+
+    return render(request, 'blog/home.html')
