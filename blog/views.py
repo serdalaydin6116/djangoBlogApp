90cae5d64e7b3408620b42d9c3b1b6698a4ff7d1
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-07 21:42:56 +0300

    create and update views were created

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post 
 from .forms import PostForm
@@ -14,6 +14,41 @@ def home(request):
     }
     return render(request, 'blog/post_list.html', context)
 
+
+def blog_create(request):
+    form=PostForm()
+    if request.method=='POST':
+        form=PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    context={
+        'form' :form
+    }
+    return render(request, 'blog/post_create.html', context)
+
+
+def blog_update (request, id):
+    post= Post.objects.get(id=id)
+    form=PostForm(instance=post)
+    if request.method=='POST':
+        form=PostForm(request.POST,instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context ={ 
+        
+        'form': form,
+    }
+    return render(request,'blog/post_update.html', context )
+
+def blog_delete(request, id):
+    post=Post.objects.get(id=id)
+    if request.method=='POST':
+        post.delete()
+        return redirect ('home')
+    return render (render, 'blog/post_delete')
 
     
 
