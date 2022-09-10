88d25d0e4d196e28bf20604829a2b932a602616e
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-10 19:02:28 +0300

    password_change vwas created, urls  formed

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import login, authenticate
 
 def home(request):
@@ -37,6 +37,24 @@ def register(request):
             'form': form
     }
     return render(request, "registration/register.html", context)
+
+
+def password_change(request):
+    if request.method == 'POST':
+        # We will use user change form this time
+        # Import it
+        form = UserChangeForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        form = UserChangeForm()
+    
+    context = {
+        'form': form
+    }
+    
+    return render(request, "registration/password_change.html", context)
 
 
 
